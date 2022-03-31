68e07192e6b4e5301af2f44b32e34dd961aac147
Suszek20p <91263488+Suszek20p@users.noreply.github.com>
2022-03-31 09:30:29 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+        if (!empty($_POST['login']) && !empty($_POST['password']) && !empty($_POST['email']))
+        {
+          echo "Twoj login to: " .$_POST['login']."</br>". "Twoje haslo to: ".$_POST['password']."</br>". "Twoj email to: ".$_POST['email'];
+        }
+?>
